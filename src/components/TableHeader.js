5e12c2cd90f27ff6c5aa3e718807b51d8091bdf3
aditi <aditi.agarwal@amadeus.com>
2020-08-09 22:36:53 +0530

    Sorting added

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './Table.module.css';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const TableHeader=(props)=>{
+
+const TableHeader = (headers, onSorting) => {
+
+    const [sortingField, setSortingField] = useState("");
+    const [sortingOrder, setSortingOrder] = useState("asc");
+
+    const onSortingChange = field => {
+        const order =
+            field === sortingField && sortingOrder === "asc" ? "desc" : "asc";
+
+        setSortingField(field);
+        setSortingOrder(order);
+        headers.onSorting(field, order);
+    }
+
     return (
         <div className={classes.header}>
-           {props.headers.map(head=>(
-                <div className={classes.col} key={head.name} >
-                            {head.field}
-                            
+            {headers.headers.map(({ name, field, sortable }) => (
+                <div className={classes.col}
+                    key={name}
+                    onClick={() => sortable ? onSortingChange(field) : null}
+                >
+
+                    {field}
+
+                    {/* {sortingField && sortingField === field && (
+                        <FontAwesomeIcon
+                            icon={
+                                sortingOrder === "asc"
+                                    ? "arrow-down"
+                                    : "arrow-up"
+                            }
+                        />
+                    )} */}
                 </div>
             ))}
-          
+
         </div>
     )
 }
--- src/components/table.js
+++ src/components/table.js
@@ -4,58 +4,81 @@ import classes from './Table.module.css';
 import TableHeader from './TableHeader';
 import Search from './Search';
 
-const Table=()=> {
+const Table = () => {
   let [user, setUser] = useState([]);
   let [search, setSearch] = useState("");
+  const [sorting, setSorting] = useState({ field: "", order: "" });
 
-   var header = [
-    {name:"id",field:"ID"},
-    {name:"name",field:"NAME"},
-    {name:"email",field:"EMAIL"},
-    {name:"username",field:"USERNAME"},
-    {name:"city",field:"CITY"}
+  var headers = [
+    { name: "id", field: "ID", sortable: false },
+    { name: "name", field: "NAME", sortable: true },
+    { name: "email", field: "EMAIL", sortable: true },
+    { name: "username", field: "USERNAME", sortable: true },
+    { name: "city", field: "CITY", sortable: false }
   ];
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      setUser(response.data) 
-    });
-}, []);
- 
+      .then(response => {
+        setUser(response.data)
+      });
+  }, []);
+
   const userData = useMemo(() => {
     let computedData = user;
+
+    //Searching Data
     if (search) {
-          computedData = computedData.filter(
-                users =>
-                    users.name.toLowerCase().includes(search.toLowerCase()) ||
-                    users.email.toLowerCase().includes(search.toLowerCase())
-            );
+      computedData = computedData.filter(
+        users =>
+          users.name.toLowerCase().includes(search.toLowerCase()) ||
+          users.email.toLowerCase().includes(search.toLowerCase())
+      );
     }
+
+    //Sorting Data
+    if (sorting.field) {
+      const reversed = sorting.order === "asc" ? 1 : -1;
+      // var fld = sorting.field.toLocaleLowerCase();
+      computedData = computedData.sort(
+        (a, b) => {
+          var fld = sorting.field.toLocaleLowerCase();
+          a = a[fld];
+          b = b[fld];
+          return reversed * a.localeCompare(b)
+        }
+      );
+    }
+
     return computedData;
-}, [user, search]);
+  }, [user, search, sorting]);
 
-  var posts =userData.map(post => {
-      return (
-        <div className={classes.row} key={post.id}>
-          <div className={classes.col}>{post.id}</div>
-          <div className={classes.col}>{post.name}</div>
-          <div className={classes.col}>{post.email}</div>
-          <div className={classes.col}>{post.username}</div>
-          <div className={classes.col}>{post.address.city}</div>
-        </div>
-      )
-    });
-
+  var posts = userData.map(post => {
     return (
-      <div className={classes.main}>
-         <h1>React Data Table</h1>
-         <Search onSearch={value => {
-                  setSearch(value);}}/>
-         <TableHeader headers={header} />
-         {posts}   
-      </div >
+      <div className={classes.row} key={post.id}>
+        <div className={classes.col}>{post.id}</div>
+        <div className={classes.col}>{post.name}</div>
+        <div className={classes.col}>{post.email}</div>
+        <div className={classes.col}>{post.username}</div>
+        <div className={classes.col}>{post.address.city}</div>
+      </div>
     )
-  }
+  });
+
+  return (
+    <div className={classes.main}>
+      <h1>React Data Table</h1>
+      <Search onSearch={value => {
+        setSearch(value);
+      }} />
+      <TableHeader headers={headers}
+        onSorting={(field, order) =>
+          setSorting({ field, order })
+        }
+      />
+      {posts}
+    </div >
+  )
+}
 
 export default Table;
